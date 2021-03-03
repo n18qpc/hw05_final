a06b8ba2e9b8da3f2689dd772de9f9df18ffa839
Максим Егоров <egorovmaksima@gmail.com>
2021-03-03 23:00:50 +0500

    Final project fixies

--- yatube/settings.py
+++ yatube/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
     'sorl.thumbnail',
 ]
 
@@ -55,7 +56,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+] 
 
 ROOT_URLCONF = 'yatube.urls'
 
